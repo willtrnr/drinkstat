461c2652b5f5b485d1daa572840e32aff760d23c
Zi Long <zilong0206@hotmail.com>
2016-02-28 13:27:25 -0500

    added heatmap

--- src/views/BillingView/BillingView.js
+++ src/views/BillingView/BillingView.js
@@ -8,6 +8,17 @@ export default class BillingView extends React.Component {
   }
 
   render () {
-    return (<div>HELLO KITTY</div>)
+    return (
+        <View row width='auto'>
+          <Paper style={{ margin: '15px', padding: '10px 40px' }}>
+            {(this.state.stats) ? <LineChart
+              data={this.state.stats}
+              width={1000}
+              height={400}
+              title='Level Chart'/> : <LinearProgress />}
+          </Paper>
+        </View>
+        //<img src="credit_card.png"/>
+    )
   }
 }
