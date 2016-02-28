a88dcf11a862a9cef1881b7c55adf0a6a3993b96
Zi Long <zilong0206@hotmail.com>
2016-02-28 13:02:38 -0500

    HELLO KITTY IN BillingView

---
+++ src/views/BillingView/BillingView.js
@@ -0,0 +1,13 @@
+/* @flow */
+import React from 'react'
+
+export default class BillingView extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (<div>HELLO KITTY</div>)
+  }
+}
